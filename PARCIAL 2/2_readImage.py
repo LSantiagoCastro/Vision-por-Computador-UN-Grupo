253f5c1d19673315cdc291ab935028d51cca5975
scastrov99 <lcastrov@unal.edu.co>
2022-05-26 15:38:59 -0500

    Modulos parcial 2

---
+++ PARCIAL 2/2_readImage.py
@@ -0,0 +1,24 @@
+"""Script to take read image from disk and display it 
+
+Usage:
+    2_readImage.py <path>
+"""
+from docopt import docopt
+import funciones 
+
+import cv2
+
+def main(path_image):
+    image =  cv2.imread(path_image)
+    im = funciones.detect_colordetect_color(image)
+    conteo = im.Conteo_total()
+
+    
+    cv2.imshow('image',conteo)
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    path_image = args['<path>']
+    main(path_image)
